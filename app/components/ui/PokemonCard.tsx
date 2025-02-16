3225dfd5019cf9f53e7c1c115b15696824431083
angelxescomx08 <69997011+angelxescomx08@users.noreply.github.com>
2025-02-16 10:23:00 -0600

    refactor: update PokemonCard component to use URL instead of image property

--- app/components/ui/PokemonCard.tsx
+++ app/components/ui/PokemonCard.tsx
@@ -4,20 +4,16 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import CardActionArea from '@mui/material/CardActionArea';
+import { Pokemon } from '@/app/interfaces/pokemon';
 
-type Pokemon = {
-  name: string;
-  image: string;
-}
-
-export default function PokemonCard({ name, image }:Pokemon) {
+export default function PokemonCard({ name, url }:Pokemon) {
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardActionArea>
         <CardMedia
           component="img"
           height="140"
-          image={image}
+          image={url}
           alt="green iguana"
         />
         <CardContent>
