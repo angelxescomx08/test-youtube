a4c5da60252c2e18454c7961797418349bb7ef5d
angelxescomx08 <69997011+angelxescomx08@users.noreply.github.com>
2025-02-16 10:10:27 -0600

    add pokemon card and clear home

--- app/page.tsx
+++ app/page.tsx
@@ -1,41 +1,8 @@
-import { ReactNode } from "react";
-
-type Props<T> = {
-  items: T[];
-  renderItem: (item: T)=>ReactNode
-}
-
-function List<T>({ items, renderItem }:Props<T>){
-  return (
-    <ul>
-      {
-        items.map((item,index)=>(
-          <li key={index}>
-            {renderItem(item)}
-          </li>
-        ))
-      }
-    </ul>
-  )
-}
 
 export default function Home() {
   return (
-    <main>
-      <List items={[
-        {
-          id: 1,
-          name: "Laptop"
-        },
-        {
-          id: 2,
-          name: "Cellphone"
-        },
-        {
-          id: 3,
-          name: "Tablet"
-        },
-      ]} renderItem={(product)=>product.name} />
+    <main className="container m-auto">
+
     </main>
   );
 }
