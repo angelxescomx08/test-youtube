7d430262837d0d1450b7f63fc9df55857c5960a3
angelxescomx08 <69997011+angelxescomx08@users.noreply.github.com>
2025-03-30 10:09:51 -0600

    feat: enhance Counter component with user search and filter functionality

--- app/components/Counter.tsx
+++ app/components/Counter.tsx
@@ -1,41 +1,79 @@
-import { Button } from '@mui/material'
-import { Ref, useImperativeHandle, useState } from 'react'
+import { Button, TextField } from "@mui/material";
+import { memo, useState } from "react";
 
-export type CounterRef = {
-  count: ()=>number;
-  reset: VoidFunction;
-}
+const users = [
+	"Ellen Turner",
+	"Jim Steele",
+	"Hattie Higgins",
+	"Willie Walker",
+	"Warren Zimmerman",
+	"Ruth Kennedy",
+	"Lee Gray",
+	"Amy Shaw",
+	"Lucas Wong",
+	"Randy Barnes",
+	"Lucille Dunn",
+	"Ian Foster",
+	"Lester Wise",
+	"Mable Matthews",
+	"Lawrence Warren",
+];
 
-type Props = {
-  counterRef: Ref<CounterRef>
-}
+const UsersList = ({ search, filter }: { search: string; filter: string }) => {
+	console.log("Se renderizó la lista de usuarios");
+	return (
+		<div>
+			<p className="text-white font-semibold">search: {search}</p>
+			<p className="text-white font-semibold mb-5">filter: {filter}</p>
+			<ul>
+				{users
+					.filter((user) => user.toLowerCase().includes(search.toLowerCase()))
+					.map((user, index) => (
+						<li key={index} className="text-white">
+							{user}
+						</li>
+					))}
+			</ul>
+		</div>
+	);
+};
 
-export const Counter = ({ counterRef }:Props) => {
+const UsersListMemo = memo(UsersList, (prevProps, nextProps) => {
+	return prevProps.search === nextProps.search;
+});
 
-  const [count,setCount] = useState(0)
+export const Counter = () => {
+	const [count, setCount] = useState(0);
+	const [search, setSearch] = useState("");
+	const [filter, setFilter] = useState("");
 
-  useImperativeHandle(counterRef,()=>({
-    count: ()=> count,
-    reset: ()=>setCount(0)
-  }),[count])
-  
-  return (
-    <div>
-      <p className='text-white font-medium text-xl'>{count}</p>
-      <div className='flex items-center gap-4'>
-        <Button
-          className='button'
-          onClick={()=>setCount(count-1)}
-        >
-          -1
-        </Button>
-        <Button
-          className='button'
-          onClick={()=>setCount(count+1)}
-        >
-          +1
-        </Button>
-      </div>
-    </div>
-  )
-}
+	console.log("Se renderizó el contador");
+	return (
+		<div className="flex flex-col gap-4">
+			<div className="flex items-center gap-4">
+				<Button className="button" onClick={() => setCount(count - 1)}>
+					-1
+				</Button>
+				<p className="text-white font-medium text-xl">{count}</p>
+				<Button className="button" onClick={() => setCount(count + 1)}>
+					+1
+				</Button>
+			</div>
+			<hr />
+			<TextField
+				className="bg-white"
+				placeholder="search..."
+				value={search}
+				onChange={(e) => setSearch(e.target.value)}
+			/>
+			<TextField
+				className="bg-white"
+				placeholder="filter..."
+				value={filter}
+				onChange={(e) => setFilter(e.target.value)}
+			/>
+
+			<UsersListMemo search={search} filter={filter} />
+		</div>
+	);
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,82 +1,13 @@
 "use client";
 
-import { Button } from "@mui/material";
-import { useState } from "react";
-
-function calculateState() {
-	const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-	return arr.reduce((prev, current) => prev + current, 0);
-}
+import { Counter } from "./components/Counter";
 
 export default function Home() {
-	const [count, setCount] = useState(calculateState);
-	const [user, setUser] = useState({
-		name: "My user",
-		age: 20,
-	});
-
 	return (
 		<main className="container m-auto p-5">
 			<div className="flex items-center gap-4">
-				<Button
-					className="button"
-					onClick={() => {
-						setCount(count + 5);
-						setCount(count + 5);
-					}}
-				>
-					-10
-				</Button>
-				<p className="text-white">{count}</p>
-				<Button
-					className="button"
-					onClick={() => {
-						setCount((prev) => prev + 5);
-						setCount((prev) => prev + 5);
-					}}
-				>
-					+10
-				</Button>
+				<Counter />
 			</div>
-			{/* <div className="flex flex-col gap-4 w-40">
-				<input
-					type="text"
-					placeholder="name"
-					value={user.name}
-					onChange={(e) => {
-						setUser((prev) => ({
-							...prev,
-							name: e.target.value,
-						}));
-					}}
-				/>
-				<input
-					type="text"
-					placeholder="age"
-					value={user.age}
-					onChange={(e) => {
-						setUser((prev) => ({
-							...prev,
-							age: Number(e.target.value),
-						}));
-					}}
-				/>
-			</div>
-
-			<p className="text-white font-semibold">name: {user.name}</p>
-			<p className="text-white font-semibold">age: {user.age}</p>
-
-			<Button
-				className="button mt-4"
-				onClick={() => {
-					setUser((prev) => ({
-						...prev,
-						name: "Jose",
-					}));
-				}}
-			>
-				Change name
-			</Button> */}
 		</main>
 	);
 }
