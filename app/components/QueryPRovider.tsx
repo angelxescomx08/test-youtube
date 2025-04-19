bece90c1fd31b48cdc6df4d35e4757f9b1990fdb
ahxdevelopmx <91965647+ahxdevelopmx@users.noreply.github.com>
2025-04-18 22:53:05 -0600

    feat: server components

--- app/components/QueryPRovider.tsx
+++ app/components/QueryPRovider.tsx
@@ -1,14 +1,12 @@
-"use client"
+"use client";
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import React, { PropsWithChildren } from 'react'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React, { PropsWithChildren } from "react";
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
-export const QueryProvider = ({ children }:PropsWithChildren) => {
-  return (
-    <QueryClientProvider client={queryClient}>
-      {children}
-    </QueryClientProvider>
-  )
-}
+export const QueryProvider = ({ children }: PropsWithChildren) => {
+	return (
+		<QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+	);
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,32 @@
-"use client";
+import { Post } from "./interfaces/posts";
 
-import StopwatchWithLeak from "./components/RefComponents";
+export default async function Home() {
+	const posts = await fetch("http://localhost:3000/api/posts")
+		.then((res) => res.json())
+		.then((data) => data as Post[])
+		.catch((err) => []);
 
-export default function Home() {
+	if (posts.length === 0) {
+		return (
+			<div>
+				<h1 className="text-center text-2xl">No posts found</h1>
+			</div>
+		);
+	}
+
 	return (
 		<main className="container m-auto p-5">
-			<div className="flex items-center gap-4">
-				<StopwatchWithLeak />
+			<div className="flex flex-wrap gap-4">
+				{posts.map((post) => (
+					<div
+						key={post.id}
+						className="bg-slate-900 rounded-lg p-5 m-2 shadow-lg w-96"
+					>
+						<h2 className="text-white text-2xl">{post.title}</h2>
+						<p className="text-white mt-2">{post.content}</p>
+						<p className="text-white mt-2 italic">{post.date}</p>
+					</div>
+				))}
 			</div>
 		</main>
 	);
